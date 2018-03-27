520eb8123dc6626dc1f629384785a357bc26173b
= <jenjennychen@gmail.com>
2018-03-27 19:25:30 -0400

    images update

--- app.js
+++ app.js
@@ -17,144 +17,126 @@ const ComedyBtn = document.getElementById('comedy');
 const ActionBtn = document.getElementById('action');
 const HorrorBtn = document.getElementById('horror');
 const DocumentaryBtn = document.getElementById('documentary');
-//Question 4
+//Question 4 dog
 
-//Question 5
-//Question 6
+//Question 5 cat
 
-//const images = [
-//    "image/cat1.jpg",
-//    "image/cat2.jpg",
-//    "image/cat3.png",
-//    "image/cat4.jpg",
-//	"image/dog1.jpg",
-//    "image/dog2.jpg",
-//    "image/dog3.png",
-//    "image/dog4.jpg"
-//];
 
 var personality = {
-	happy: 0,
-	sad: 0,
-	dogs: -1
+	Introvert: 0,
+	Extrovert: 0,
 };
 
-document.getElementById('question1').style.display = "block";
+document.getElementById('question1').style.display = "none";
 document.getElementById('question2').style.display = "none";
 document.getElementById('question3').style.display = "none";
 document.getElementById('question4').style.display = "none";
 document.getElementById('question5').style.display = "none";
+document.getElementById('results').style.display = "none";
+
 
 
 // start
 const start = document.getElementById('start').value;
 startBtn.onclick = function () {
 	document.getElementById('question1').style.display = "block";
-	//	document.getElementById('answer').style.display = "none";
 	document.getElementById('start').style.display = "none";
+	document.getElementById("banner").remove();
+}
+//question 1 color
+function getColor(event) {
+	const question1Container = document.getElementById('question1');
+	const color = event.target.value;
+
+	if (color === 'yellow') {
+		personality.Introvert += 1;
+
+	} else if (color === 'blue') {
+		personality.Extrovert += 1;
+
+	} else if (color === 'green') {
+		personality.Introvert += 1;
+
+	} else if (color === 'purple') {
+		personality.Extrovert += 1;
+
+	} else if (color === 'red') {
+		personality.Introvert += 1;
+
+	} else if (color === 'orange') {
+		personality.Extrovert += 1;
+
+	}
+
+	//	START
+
+	document.getElementById('question1').style.display = "none";
+	document.getElementById('question2').style.display = "block";
 }
 
-//question 1 color
-YellowBtn.onclick = function () {
-	const anwser = document.getElementById('question1').value;
-	document.getElementById('question1').style.display = "none";
-	//	document.getElementById('answer').style.display = "none";
-	document.getElementById('question2').style.display = "block";
-};
+YellowBtn.addEventListener('click', getColor);
+GreenBtn.addEventListener('click', getColor);
+PurpleBtn.addEventListener('click', getColor);
+RedBtn.addEventListener('click', getColor);
+PurpleBtn.addEventListener('click', getColor);
+OrangeBtn.addEventListener('click', getColor);
+BlueBtn.addEventListener('click', getColor);
 
-GreenBtn.onclick = function () {
-	const anwser = document.getElementById('question1').value;
-	document.getElementById('question1').style.display = "none";
-	//	document.getElementById('answer').style.display = "none";
-	document.getElementById('question2').style.display = "block";
-};
-PurpleBtn.onclick = function () {
-	const anwser = document.getElementById('question1').value;
-	document.getElementById('question1').style.display = "none";
-	//	document.getElementById('answer').style.display = "none";
-	document.getElementById('question2').style.display = "block";
-};
-RedBtn.onclick = function () {
-	const anwser = document.getElementById('question1').value;
-	document.getElementById('question1').style.display = "none";
-	//	document.getElementById('answer').style.display = "none";
-	document.getElementById('question2').style.display = "block";
-};
-PurpleBtn.onclick = function () {
-	const anwser = document.getElementById('question1').value;
-	document.getElementById('question1').style.display = "none";
-	//	document.getElementById('answer').style.display = "none";
-	document.getElementById('question2').style.display = "block";
-};
-OrangeBtn.onclick = function () {
-	const anwser = document.getElementById('question1').value;
-	document.getElementById('question1').style.display = "none";
-	//	document.getElementById('answer').style.display = "none";
-	document.getElementById('question2').style.display = "block";
-};
-BlueBtn.onclick = function () {
-	const anwser = document.getElementById('question1').value;
-	document.getElementById('question1').style.display = "none";
-	//	document.getElementById('answer').style.display = "none";
-	document.getElementById('question2').style.display = "block";
-};
+//question 2 mood
+function getMood(event) {
+	const mood = event.target.value;
 
-//question 2
-AnnoyedBtn.onclick = function () {
-	const anwser = document.getElementById('question2').value;
+	if (mood === 'annoyed') {
+		personality.Introvert += 1;
+		console.log('mood');
+	} else if (mood === 'rejected') {
+		personality.Extrovert += 1;
+
+	} else if (mood === 'relieved') {
+		personality.Introvert += 1;
+
+	} else if (mood === 'whatever') {
+		personality.Extrovert += 1;
+
+	}
+
 	document.getElementById('question2').style.display = "none";
-	//	document.getElementById('answer').style.display = "none";
 	document.getElementById('question3').style.display = "block";
-};
+}
 
-RejectedBtn.onclick = function () {
-	const anwser = document.getElementById('question2').value;
-	document.getElementById('question2').style.display = "none";
-	//	document.getElementById('answer').style.display = "none";
-	document.getElementById('question3').style.display = "block";
-};
+AnnoyedBtn.addEventListener('click', getMood);
+RejectedBtn.addEventListener('click', getMood);
+RelievedBtn.addEventListener('click', getMood);
+WhateverBtn.addEventListener('click', getMood);
 
-RelievedBtn.onclick = function () {
-	const anwser = document.getElementById('question2').value;
-	document.getElementById('question2').style.display = "none";
-	//	document.getElementById('answer').style.display = "none";
-	document.getElementById('question3').style.display = "block";
-};
-
-WhateverBtn.onclick = function () {
-	const anwser = document.getElementById('question2').value;
-	document.getElementById('question2').style.display = "none";
-	//	document.getElementById('answer').style.display = "none";
-	document.getElementById('question3').style.display = "block";
-};
 
 //question 3
-ComedyBtn.onclick = function () {
-	const anwser = document.getElementById('question3').value;
-	document.getElementById('question3').style.display = "none";
-	//	document.getElementById('answer').style.display = "none";
-	document.getElementById('question4').style.display = "block";
-};
-ActionBtn.onclick = function () {
-	const anwser = document.getElementById('question3').value;
-	document.getElementById('question3').style.display = "none";
-	//	document.getElementById('answer').style.display = "none";
-	document.getElementById('question4').style.display = "block";
-};
+function getMovie(event) {
+	const movie = event.target.value;
 
-HorrorBtn.onclick = function () {
-	const anwser = document.getElementById('question3').value;
-	document.getElementById('question3').style.display = "none";
-	//	document.getElementById('answer').style.display = "none";
-	document.getElementById('question4').style.display = "block";
-};
+	if (movie === 'comedy') {
+		personality.Introvert += 1;
+//		console.log('movie');
+	} else if (movie === 'action') {
+		personality.Extrovert += 1;
 
-DocumentaryBtn.onclick = function () {
-	const anwser = document.getElementById('question3').value;
+	} else if (movie === 'horror') {
+		personality.Introvert += 1;
+
+	} else if (movie === 'documentary') {
+		personality.Extrovert += 1;
+
+	}
+
 	document.getElementById('question3').style.display = "none";
-	//	document.getElementById('answer').style.display = "none";
 	document.getElementById('question4').style.display = "block";
-};
+}
+
+ComedyBtn.addEventListener('click', getMovie);
+ActionBtn.addEventListener('click', getMovie);
+HorrorBtn.addEventListener('click', getMovie);
+DocumentaryBtn.addEventListener('click', getMovie);
+
 
 //question 4
 const images = [
@@ -163,118 +145,89 @@ const images = [
     "images/dog3.jpg",
     "images/dog4.jpg"
 ];
-const captions = [
-    "puppy 1",
-    "puppy 2",
-    "puppy 3",
-    "puppy 4"
-];
 
-const anwser = document.getElementById('question4').value;
-const container = document.getElementById("question4");
+//const captions = [
+//    "puppy 1",
+//    "puppy 2",
+//    "puppy 3",
+//    "puppy 4",
+//	"kitten 1",
+//    "kitten 2",
+//    "kitten 3",
+//    "kitten 4"
+//];
+
+//question 4 dog
+
+const answser = document.getElementById('question4').value;
+const container = document.querySelector(".dogImgContainer");
 for (let i = 0; i < 4; i++) {
 
 	const imgContainer = document.createElement('div');
-
+	imgContainer.classList.add("dogs");
 	const img = new Image();
 	img.src = images[i];
 	img.id = "img" + i;
 	img.classList.add("slide");
 
-	img.onclick = function() {
+	img.onclick = function () {
 		personality.dogs = i;
-		
+
 		document.getElementById('question4').style.display = "none";
 		document.getElementById('question5').style.display = "block";
 	};
-	
-	
-//	images[0].onclick = function () {
-//		console.log(this);
-//		personality.dog1++;
-//		document.getElementById('question4').style.display = "none";
-//		document.getElementById('question5').style.display = "block";
-//	};
-//	images[1].onclick = function () {
-//		personality.dog2++;
-//		document.getElementById('question4').style.display = "none";
-//		document.getElementById('question5').style.display = "block";
-//	};
-//	images[2].onclick = function () {
-//		personality.dog3++;
-//		document.getElementById('question4').style.display = "none";
-//		document.getElementById('question5').style.display = "block";
-//	};
-//	images[3].onclick = function () {
-//		personality.dog4++;
-//		document.getElementById('question4').style.display = "none";
-//		document.getElementById('question5').style.display = "block";
-//	};
 
 	imgContainer.appendChild(img);
 
-	const caption = document.createElement('p');
-	caption.classList.add("captions");
-	caption.textContent = captions[i];
-	imgContainer.appendChild(caption);
-
-	container.appendChild(imgContainer);
-};
-
-//question 5
-//const images = [
-//	"images/cat1.jpg",
-//    "images/cat2.jpg",
-//    "images/cat3.jpg",
-//    "images/cat4.jpg"
-//];
-//const captions = [
-//    "kitten 1",
-//    "kitten 2",
-//    "kitten 3",
-//    "kitten 4"
-//];
-//
-//const anwser = document.getElementById('question5').value;
-//const container = document.getElementById("question5");
-//for (let i = 0; i < 4; i++) {
-//
-//	const imgContainer = document.createElement('div');
-//
-//	const img = new Image();
-//	img.src = images[i];
-//	img.id = "img" + i;
-//	img.classList.add("slide");
-//	images[i];
-//
-//	images[0].onclick = function () {
-//		personality.cat1++;
-//		document.getElementById('question5').style.display = "none";
-//		document.getElementById('question5').style.display = "block";
-//	};
-//	images[1].onclick = function () {
-//		personality.cat2++;
-//		document.getElementById('question5').style.display = "none";
-//		document.getElementById('question5').style.display = "block";
-//	};
-//	images[2].onclick = function () {
-//		personality.cat3++;
-//		document.getElementById('question5').style.display = "none";
-//		document.getElementById('question5').style.display = "block";
-//	};
-//	images[3].onclick = function () {
-//		personality.cat4++;
-//		document.getElementById('question5').style.display = "none";
-//		document.getElementById('question5').style.display = "block";
-//	};
-//
-//	imgContainer.appendChild(img);
-//
 //	const caption = document.createElement('p');
 //	caption.classList.add("captions");
 //	caption.textContent = captions[i];
 //	imgContainer.appendChild(caption);
-//
-//	container.appendChild(imgContainer);
-//};
-//
+
+	container.appendChild(imgContainer);
+	
+};
+
+
+const catImages = [
+	"images/cat1.jpg",
+    "images/cat2.jpg",
+    "images/cat3.jpg",
+    "images/cat4.jpg"
+];
+
+//question 5 cat
+const container5 = document.querySelector(".catImgContainer");
+for (let i = 0; i < catImages.length; i++) {
+
+	const imgContainer = document.createElement('div');
+	imgContainer.classList.add("cats");
+
+	const img = document.createElement("img");
+	img.setAttribute("src", catImages[i]);
+	img.id = "catImage" + i;
+	imgContainer.appendChild(img);
+	container5.appendChild(imgContainer);
+	img.addEventListener('click', function (event) {
+		const imgId = event.target.id;
+		if (imgId === 'catImage0') {
+			personality.Introvert += 1;
+			console.log('You clicked a cute cat!');
+		} else if (imgId === 'catImage1') {
+			personality.Extrovert += 1;
+
+		} else if (imgId === 'catImage2') {
+			personality.Extrovert += 1;
+
+		} else if (imgId === 'catImage3') {
+			personality.Introvert += 1;
+		} else if (imgId === 'catImage4') {
+			personality.Extrovert += 1;
+		}
+		document.getElementById('question5').style.display = 'none';
+		document.getElementById('results').style.display = 'none';
+		
+	});
+	
+}
+
